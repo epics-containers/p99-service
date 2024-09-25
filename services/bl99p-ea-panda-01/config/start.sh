aecedc9289f5a1f7853e682305a05c5f923bd2ce
Max Herbert <max.herbert@diamond.ac.uk>
2024-09-25 13:41:05 +0100

    Changed IP

--- services/bl99p-ea-panda-01/config/start.sh
+++ services/bl99p-ea-panda-01/config/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandablocks-ioc softioc 192.168.250.18 BL99P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
+pandablocks-ioc softioc 10.177.8.1  BL99P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
