a42031cec1fd049d7f67f450c296bb898b1286d8
Max Herbert <max.herbert@diamond.ac.uk>
2024-10-09 15:26:44 +0100

    Minor changes to panda and positioner

--- services/bl99p-ea-panda-01/config/start.sh
+++ services/bl99p-ea-panda-01/config/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandablocks-ioc softioc 10.177.8.1 BL99P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
+pandablocks-ioc softioc 10.177.8.1 BL99P-MO-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
